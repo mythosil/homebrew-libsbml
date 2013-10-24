bcd9cbf7d3a66e8b4f6eeef63f07407566d0692d
mythosil <mythosil@yahoo.co.jp>
2013-10-25 02:03:10 +0900

    remove pkg-config from dependency

--- libsbml.rb
+++ libsbml.rb
@@ -5,8 +5,6 @@ class Libsbml < Formula
   url 'http://sourceforge.net/projects/sbml/files/libsbml/5.8.0/stable/libSBML-5.8.0-src.tar.gz'
   sha1 'ca931f75962f3bb4397f96259c338185014ff091'
 
-  depends_on 'pkg-config' => :build
-
   def install
     system "./configure", "--disable-debug",
                           "--disable-silent-rules",
