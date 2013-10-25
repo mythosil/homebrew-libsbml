27dc154f43fcd5ce1d3c3c185e79713f45ca95a8
mythosil <mythosil@yahoo.co.jp>
2013-10-26 01:26:44 +0900

    add version

--- libsbml.rb
+++ libsbml.rb
@@ -3,6 +3,7 @@ require 'formula'
 class Libsbml < Formula
   homepage 'http://sbml.org/Software/libSBML'
   url 'http://sourceforge.net/projects/sbml/files/libsbml/5.8.0/stable/libSBML-5.8.0-src.tar.gz'
+  version '5.8.0'
   sha1 'ca931f75962f3bb4397f96259c338185014ff091'
 
   def install
