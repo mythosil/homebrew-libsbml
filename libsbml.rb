b5d4c5b993a7d49c953a87d9f5528fa52ba89b43
mythosil <mythosil@yahoo.co.jp>
2013-12-08 01:42:21 +0900

    upgrade to 5.9.0

--- libsbml.rb
+++ libsbml.rb
@@ -2,14 +2,37 @@ require 'formula'
 
 class Libsbml < Formula
   homepage 'http://sbml.org/Software/libSBML'
-  url 'http://sourceforge.net/projects/sbml/files/libsbml/5.8.0/stable/libSBML-5.8.0-src.tar.gz'
-  version '5.8.0'
-  sha1 'ca931f75962f3bb4397f96259c338185014ff091'
+  url 'http://sourceforge.net/projects/sbml/files/libsbml/5.9.0/stable/libSBML-5.9.0-core-plus-packages-src.zip'
+  version '5.9.0'
+  sha1 '91305920fa12db48d3ae7f6ea7f272415ed97616'
+
+  depends_on 'cmake' => :build
+  depends_on 'swig' => :build
+
+  option 'with-csharp', 'generate C# interface library [default=no]'
+  option 'with-java', 'generate Java interface library [default=no]'
+  option 'with-matlab', 'generate MATLAB interface library [default=no]'
+  option 'with-octave', 'generate Octave interface library [default=no]'
+  option 'with-perl', 'generate Perl interface library [default=no]'
+  option 'with-python', 'generate Python interface library [default=no]'
+  option 'with-ruby', 'generate Ruby interface library [default=no]'
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    system "make", "install"
+    mkdir 'build' do
+      args = std_cmake_args
+      args << "-DCMAKE_INSTALL_PREFIX=#{prefix}"
+
+      args << '-DWITH_CSHARP=ON' if build.with? 'csharp'
+      args << '-DWITH_JAVA=ON' if build.with? 'java'
+      args << '-DWITH_MATLAB=ON' if build.with? 'matlab'
+      args << '-DWITH_OCTAVE=ON' if build.with? 'octave'
+      args << '-DWITH_PERL=ON' if build.with? 'perl'
+      args << '-DWITH_PYTHON=ON' if build.with? 'python'
+      args << '-DWITH_RUBY=ON' if build.with? 'ruby'
+
+      system 'cmake', '..', *args
+      ENV.deparallelize # just for debug
+      system 'make', 'install'
+    end
   end
 end
