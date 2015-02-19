41cf4b989fe93cf586bf7c159b87938365052d7c
Akito Tabira <mythosil@yahoo.co.jp>
2015-02-19 23:26:17 +0900

    replace single-quote with double-quote

--- libsbml.rb
+++ libsbml.rb
@@ -1,56 +1,56 @@
 class Libsbml < Formula
-  homepage 'http://sbml.org/Software/libSBML'
-  url 'http://sourceforge.net/projects/sbml/files/libsbml/5.11.0/stable/libSBML-5.11.0-core-plus-packages-src.tar.gz'
-  version '5.11.0'
-  sha1 'cfe9697bf9e28758c0dbd7b7c6b6ad3a7a01c56e'
+  homepage "http://sbml.org/Software/libSBML"
+  url "http://sourceforge.net/projects/sbml/files/libsbml/5.11.0/stable/libSBML-5.11.0-core-plus-packages-src.tar.gz"
+  version "5.11.0"
+  sha1 "cfe9697bf9e28758c0dbd7b7c6b6ad3a7a01c56e"
 
   LANGUAGES_OPTIONAL = {
-    'csharp' => 'C#',
-    'java' => 'Java',
-    'matlab' => 'MATLAB',
-    'octave' => 'Octave',
-    'perl' => 'Perl',
-    'ruby' => 'Ruby',
-    'python' => 'Python'
+    "csharp" => "C#",
+    "java" => "Java",
+    "matlab" => "MATLAB",
+    "octave" => "Octave",
+    "perl" => "Perl",
+    "ruby" => "Ruby",
+    "python" => "Python"
   }
   LANGUAGES_OPTIONAL.each do |opt, lang|
     option "with-#{opt}", "generate #{lang} interface library [default=no]"
   end
 
-  depends_on 'cmake' => :build
-  depends_on 'swig' => :build
+  depends_on "cmake" => :build
+  depends_on "swig" => :build
   depends_on :python => :optional
 
-  # fix ruby's sitelib dir
+  # fix ruby"s sitelib dir
   def patches; DATA; end
 
   def install
-    mkdir 'build' do
+    mkdir "build" do
       args = std_cmake_args
       args << "-DCMAKE_INSTALL_PREFIX=#{prefix}"
       
-      if build.with? 'python' then
-        args << '-DWITH_PYTHON=ON'
+      if build.with? "python" then
+        args << "-DWITH_PYTHON=ON"
         args << "-DPYTHON_EXECUTABLE='#{%x(python-config --prefix).chomp}/bin/python'"
         args << "-DPYTHON_INCLUDE_DIR='#{%x(python-config --prefix).chomp}/include/python2.7'"
         args << "-DPYTHON_LIBRARY='#{%x(python-config --prefix).chomp}/lib/libpython2.7.dylib'"
       end
 
-      args << '-DWITH_CSHARP=ON' if build.with? 'csharp'
-      args << '-DWITH_JAVA=ON' if build.with? 'java'
-      args << '-DWITH_MATLAB=ON' if build.with? 'matlab'
-      args << '-DWITH_OCTAVE=ON' if build.with? 'octave'
-      args << '-DWITH_PERL=ON' if build.with? 'perl'
-      args << '-DWITH_RUBY=ON' if build.with? 'ruby'
+      args << "-DWITH_CSHARP=ON" if build.with? "csharp"
+      args << "-DWITH_JAVA=ON" if build.with? "java"
+      args << "-DWITH_MATLAB=ON" if build.with? "matlab"
+      args << "-DWITH_OCTAVE=ON" if build.with? "octave"
+      args << "-DWITH_PERL=ON" if build.with? "perl"
+      args << "-DWITH_RUBY=ON" if build.with? "ruby"
 
-      system 'cmake', '..', *args
+      system "cmake", "..", *args
       #ENV.deparallelize # uncomment for debug
-      system 'make', 'install'
+      system "make", "install"
     end
   end
 
   test do
-    (testpath/'test.c').write <<-EOS.undent
+    (testpath/"test.c").write <<-EOS.undent
       #include <sbml/SBMLTypes.h>
       int main() {
         SBMLDocument_t *d = SBMLDocument_createWithLevelAndVersion(3, 1);
@@ -58,8 +58,8 @@ class Libsbml < Formula
         return 0;
       }
     EOS
-    system ENV.cc, 'test.c', '-lsbml', '-o', 'test'
-    system './test'
+    system ENV.cc, "test.c", "-lsbml", "-o", "test"
+    system "./test"
   end
 end
 
